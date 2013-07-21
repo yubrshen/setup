d2ad1f8eb50483edbce67a0e4c57deae9ac82c11
Yu Shen <yubrshen@gmail.com>
2013-07-21 11:15:38 +0000

    add npm install for running web.js

--- setup.sh
+++ setup.sh
@@ -15,6 +15,7 @@ curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 source $HOME/.nvm/nvm.sh
 nvm install v0.10.12
 nvm use v0.10.12
+npm install #install for running node web.js properly
 
 # Install jshint to allow checking of JS code within emacs
 # http://jshint.com/
